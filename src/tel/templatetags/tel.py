d53250c70965e1ad8137514e8d20026be89b3824
Aaron Maxwell <amax@redsymbol.net>
2009-07-06 21:01:12 -0700

    do alphabetic conversion

--- src/tel/templatetags/tel.py
+++ src/tel/templatetags/tel.py
@@ -3,8 +3,45 @@ import string
 
 register = template.Library()
 
+ALPHANUM = string.digits + string.ascii_uppercase
+
+DIGIT_MAP = {
+    u'A' : u'2',
+    u'B' : u'2',
+    u'C' : u'2',
+    u'D' : u'3',
+    u'E' : u'3',
+    u'F' : u'3',
+    u'G' : u'4',
+    u'H' : u'4',
+    u'I' : u'4',
+    u'J' : u'5',
+    u'K' : u'5',
+    u'L' : u'5',
+    u'M' : u'6',
+    u'N' : u'6',
+    u'O' : u'6',
+    u'P' : u'7',
+    u'Q' : u'7',
+    u'R' : u'7',
+    u'S' : u'7',
+    u'T' : u'8',
+    u'U' : u'8',
+    u'V' : u'8',
+    u'W' : u'9',
+    u'X' : u'9',
+    u'Y' : u'9',
+    u'Z' : u'9',
+    }
+
+def is_alphanum(c):
+    return c in ALPHANUM
+
+def char_to_digit(c):
+    return DIGIT_MAP.get(c, c)
+
 def process(raw):
-    return filter(lambda c: c in string.digits, raw)
+    return u''.join(map(char_to_digit, filter(is_alphanum, raw.upper())))
 
 def tel(raw):
     return u'<a href="+1%s">%s</a>' % (process(raw), raw)
--- src/tel/tests.py
+++ src/tel/tests.py
@@ -13,6 +13,21 @@ class TestEndToEnd(unittest.TestCase):
             {'value' : u'(415)555-1212',
              'out'   : u'<a href="+14155551212">(415)555-1212</a>',
              },
+            {'value' : u'415-555-ROCK',
+             'out'   : u'<a href="+14155557625">415-555-ROCK</a>',
+             },
+            {'value' : u'415-555-rock',
+             'out'   : u'<a href="+14155557625">415-555-rock</a>',
+             },
+            {'value' : u'415-555-roCK',
+             'out'   : u'<a href="+14155557625">415-555-roCK</a>',
+             },
+            {'value' : u'415-JKL-ROCK',
+             'out'   : u'<a href="+14155557625">415-JKL-ROCK</a>',
+             },
+            {'value' : u'415-JJJ-WXYZ',
+             'out'   : u'<a href="+14155559999">415-JJJ-WXYZ</a>',
+             },
             ]
         for ii, td in enumerate(testdata):
             expected = td['out']
